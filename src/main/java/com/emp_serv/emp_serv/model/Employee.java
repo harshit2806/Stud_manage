f5a5bb3913eb1cdcfb7bc2eff8cc86e9d39e5097
Harshit <harshitsoni2806@gmail.com>
2023-03-11 20:17:21 +0530

    Initial Commit

---
+++ src/main/java/com/emp_serv/emp_serv/model/Employee.java
@@ -0,0 +1,64 @@
+package com.emp_serv.emp_serv.model;
+import java.sql.Timestamp;
+
+import javax.persistence.*;
+
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+@Entity
+@Table(name="Stud")
+public class Employee {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+   
+    @Column(name="First_Name")
+    private String firstName;
+    @Column(name="Last_Name")
+    private String lastName;
+    @Column(name="Email")
+    private String email;
+    
+	@Column (name = "Created_At", nullable = false, updatable= false)
+	@CreationTimestamp 
+	private Timestamp createdAt;
+	@Column(name = "Updated_At" )
+	@UpdateTimestamp
+	private Timestamp updatedAt;
+
+    //Getter & setter method
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+
+}
